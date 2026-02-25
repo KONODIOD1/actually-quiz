623cf4262a3418c8b7d24e8c30607ff645c32b3a
KONODIOD1 <omarfire2016@gmail.com>
2026-02-25 16:19:11 +0200

    Update game.js

--- game.js
+++ game.js
@@ -64,7 +64,7 @@ getNewQuestion = () => {
 
     if (availableQuestions.length === 0 || QuestionCounter >=MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score);
-        return window.location.assign("/end.html");
+        return window.location.assign("/final.html");
     }
     
     QuestionCounter++;
@@ -161,3 +161,4 @@ window.onload = startCountdown;
 
 
 StartGame();
+
